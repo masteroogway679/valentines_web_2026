cb2261da8b388eaab0fbec9560a80f8c795cc739
nico <nicojelua@outlook.com>
2026-01-29 23:57:58 -0500

    Fix default valentine's name format

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function validateConfig() {
     // Check required fields
     if (!config.valentineName) {
         warnings.push("Valentine's name is not set! Using default.");
-        config.valentineName = "dear s/o...";
+        config.valentineName = "dear s/o";
     }
 
     // Validate colors
